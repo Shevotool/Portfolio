eb3631ab3fb272bc6c755d9d6f15195f66fe96d9
Andrés González <89161075+Shevotool@users.noreply.github.com>
2023-05-01 15:44:59 -0600

    Carrousel

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const dotBtn1 = document.querySelector(".dot-1");
-const dotBtn2 = document.querySelector(".dot-2");
-const dotBtn3 = document.querySelector(".dot-3");
-
 const section1 = document.querySelector("#section-1");
 const section2 = document.querySelector("#section-2");
 const section3 = document.querySelector("#section-3");
@@ -11,19 +7,19 @@ const section3 = document.querySelector("#section-3");
 const navigation1 = document.querySelector("#section--1");
 const navigation2 = document.querySelector("#section--2");
 
-const dotBtn = document.querySelectorAll(".navigation__dots-btn");
 const navItems = document.querySelectorAll(".navigation__link");
 const checkbox = document.querySelector(".navigation__checkbox");
-const background = document.querySelector(".navigation__background");
 
-// cards
+// paragraph
+const paragraph = document.querySelector(".paragraph");
+const paragraph2 = document.querySelector(".paragraph2");
+
+// variables Carrousel
 const carrousel = document.querySelector(".carrousel");
 const cardsContainer = document.querySelector(".carrousel__container--cards");
 const prevBtn = document.querySelector(".carrousel__btn--prev");
 const nextBtn = document.querySelector(".carrousel__btn--sig");
 const cards = document.querySelectorAll(".card");
-
-// Carrousel
 const cardWidth = cards[0].offsetWidth;
 const cardsCount = cards.length;
 const cardContainerWidth = cardWidth * cardsCount;
@@ -31,6 +27,7 @@ let cardsShown = Math.floor(carrousel.offsetWidth / cardWidth);
 let cardsLeft = cardsCount - cardsShown;
 let cardIndex = 0;
 
+// Carrousel
 nextBtn.addEventListener("click", function () {
   if (cardIndex < cardsLeft) {
     cardIndex++;
@@ -53,70 +50,11 @@ prevBtn.addEventListener("click", function () {
 
 let selectedBtn = null;
 
-// Scroll
-
-window.addEventListener("scroll", function () {
-  const scrollPosition = window.pageYOffset;
-
-  if (
-    scrollPosition >= section1.getBoundingClientRect().top &&
-    scrollPosition < section2.getBoundingClientRect().top
-  ) {
-    updateSelectedBtn(dotBtn1);
-  } else if (
-    scrollPosition >= section2.getBoundingClientRect().top &&
-    scrollPosition < section3.getBoundingClientRect().top
-  ) {
-    updateSelectedBtn(dotBtn2);
-  } else if (scrollPosition >= section3.getBoundingClientRect().top) {
-    updateSelectedBtn(dotBtn3);
-  }
-});
-
-function updateSelectedBtn(btn) {
-  if (selectedBtn && selectedBtn !== btn) {
-    // add this condition to remove the class only if the selected button is different from the previous one
-    selectedBtn.classList.remove("navigation__dots-btn--fill");
-  }
-  selectedBtn = btn;
-  selectedBtn.classList.add("navigation__dots-btn--fill");
-}
-
-dotBtn.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    if (selectedBtn) {
-      selectedBtn.classList.remove("navigation__dots-btn--fill");
-    }
-    selectedBtn = btn;
-    selectedBtn.classList.add("navigation__dots-btn--fill");
-  });
-});
-
-// dots
-
-dotBtn1.addEventListener("click", function (e) {
-  e.preventDefault();
-  setTimeout(function () {
-    section1.scrollIntoView({ behavior: "smooth", offsetTop: 100 });
-  });
-});
-
-dotBtn2.addEventListener("click", function (e) {
-  e.preventDefault();
-  setTimeout(function () {
-    section2.scrollIntoView({ behavior: "smooth", offsetTop: 100 });
-  });
-});
-
-dotBtn3.addEventListener("click", function (e) {
-  e.preventDefault();
-  setTimeout(function () {
-    section3.scrollIntoView({ behavior: "smooth", offsetTop: 100 });
-  });
-});
+// Dark mode
+const $themeBtn = document.querySelector("#dark-mode");
+const $selectors = document.querySelectorAll("[data-dark]");
 
 // navigation
-
 document
   .querySelector(".navigation__list")
   .addEventListener("click", function (e) {
@@ -124,6 +62,8 @@ document
     if (e.target.classList.contains("navigation__link")) {
       const id = e.target.getAttribute("href");
       document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+      checkbox.checked = false;
+      background.classList.remove("navigation__background--open");
     }
   });
 
@@ -139,20 +79,76 @@ const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  //console.log(entry);
 
   if (!entry.isIntersecting) return;
 
   entry.target.classList.remove("section--hidden");
 };
 
+const THRESHOLD = 0.15;
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.15,
+  threshold: THRESHOLD,
 });
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
   //observer.unobserve(entry.target);
+});
+
+// Dark mode
+const light = "💡";
+const dark = "🔦";
+const darkBlue = "#333";
+const pink = "#faf4ee";
+
+const lightMode = () => {
+  $selectors.forEach((el) => el.classList.remove("dark-mode"));
+  $themeBtn.textContent = dark;
+  paragraph.style.color = darkBlue;
+  paragraph2.style.color = darkBlue;
+  localStorage.setItem("theme", "light");
+};
+
+const darkMode = () => {
+  $selectors.forEach((el) => el.classList.add("dark-mode"));
+  $themeBtn.textContent = light;
+  paragraph.style.color = pink;
+  paragraph2.style.color = pink;
+  localStorage.setItem("theme", "dark");
+};
+
+const toggleMode = () => {
+  if ($themeBtn.textContent === dark) {
+    darkMode();
+  } else {
+    lightMode();
+  }
+};
+
+const setMode = () => {
+  const theme = localStorage.getItem("theme") || "light";
+  if (theme === "light") {
+    lightMode();
+  } else {
+    darkMode();
+  }
+};
+
+const init = () => {
+  setMode();
+  $themeBtn.addEventListener("click", toggleMode);
+};
+
+init();
+
+//console.log(init);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const loader = document.getElementById("loader");
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 100);
 });
